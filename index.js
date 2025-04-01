be7f8aec70b020c231c649e03e5604d544926a49
estevaon <estevao.n@hotmail.com>
2025-04-01 10:29:08 -0300

    01/04

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const infosFn = async(nome) =>{
         ctx.classList.add('ctx')
         ctx = ` 
                 <div class ="login">
-                    <img src = "${data.avatar_url}" 20px>
+                    <img src = "${data.avatar_url}" style="width: 90px; height: auto;">
                     <h2>${data.name}</h2>
                     <p>login: ${data.login}</p>
                 </div>
@@ -30,21 +30,37 @@ const reposFn = async(nome) => {
     try{
         const resposta = await fetch(`https://api.github.com/users/${nome}/repos`)
         const data = await resposta.json()
+        let ctx = ''
         data.forEach((repo) => {
-            console.log(repo)
+            ctx += ` 
+                <div class ="repo">
+                    <h2>${repo.name}</h2>
+                </div>
+                <div class = "repo1">
+                    <p>Descrição: ${repo.description}</p>
+                    <p>linguagem: ${repo.language} |
+                    Forks: ${repo.forks}</p>
+                    <p> Criado em:  ${repo.created_at}</p>
+                    <p> Branch: ${repo.default_branch} </p>
+                </div>
+            `
+            document.querySelector('.repos').innerHTML = ctx
         })
+        
     }catch(err){
         console.log(err)
     }
 }
 button.addEventListener('click', () => {
     document.querySelector('.infos').innerHTML = input
+    document.querySelector('.repos').innerHTML = input
     reposFn(input.value)
     infosFn(input.value)
 })
 input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         document.querySelector('.infos').innerHTML = input
+        document.querySelector('.repos').innerHTML = input
         infosFn(input.value)
         reposFn(input.value)
     }
